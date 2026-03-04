f21e6bf6d8f45dbfb3ca7eee620a130bc12db103
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-03-03 21:20:27 -0300

    Adiciona comentários

--- Mês 03 - Março/03.03.2026/ExemploMetodoComRetorno.java
+++ Mês 03 - Março/03.03.2026/ExemploMetodoComRetorno.java
@@ -1,6 +1,15 @@
 
 public class ExemploMetodoComRetorno {
 
+	/**
+	 * Método para retornar a divisão dos valores passados como parâmetro <br>
+	 * Não tem a responsabilidade de imprimir esses valores, essa responsabilidade
+	 * fica na main
+	 * 
+	 * @param numerador   - O numerador da divisão
+	 * @param denominador - O denominador da divisão
+	 * @return O resultado da divisão
+	 */
 	public static double dividirValores(double numerador, double denominador) {
 		double retorno = numerador / denominador;
 		return retorno;
@@ -14,6 +23,10 @@ public class ExemploMetodoComRetorno {
 		double resultado = dividirValores(numerador, denominador);
 
 		System.out.println(resultado);
+
+		// Ou
+
+		System.out.println(dividirValores(numerador, denominador));
 
 	}
 
