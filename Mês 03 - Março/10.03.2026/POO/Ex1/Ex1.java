3de3cf038a7c219d4a3b023e9d558c20cf646e77
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-03-12 18:56:23 -0300

    Adiciona comentários no ex1

--- Mês 03 - Março/10.03.2026/POO/Ex1/Ex1.java
+++ Mês 03 - Março/10.03.2026/POO/Ex1/Ex1.java
@@ -3,12 +3,15 @@ public class Ex1 {
 
 	public static void main(String[] args) {
 
+		// Instância de um objeto de pessoa
 		Pessoa p1 = new Pessoa();
+		// Atribuição dos valores
 		p1.nome = "Henrique";
 		p1.sexo = 'M';
 		p1.dataNascimento = "11/10/2001";
 		p1.estadoCivil = "Solteiro";
 
+		// Sempre tem que mencionar o objeto e o atributo que quer utilizar
 		System.out.println("O seu nome é: " + p1.nome);
 
 		Pessoa p2 = new Pessoa();
--- Mês 03 - Março/10.03.2026/POO/Ex1/Pessoa.java
+++ Mês 03 - Março/10.03.2026/POO/Ex1/Pessoa.java
@@ -1,4 +1,10 @@
 
+/**
+ * Representa uma pessoa com informações pessoais básicas.
+ * 
+ * Esta classe armazena dados fundamentais sobre uma pessoa, incluindo
+ * nome, sexo, data de nascimento e estado civil.
+ */
 public class Pessoa {
 
 	String nome;
