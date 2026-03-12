913c80e6e2837bb17ea48767f4284e304b405113
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-03-12 19:38:26 -0300

    Adiciona ex3

---
+++ Mês 03 - Março/12.03.2026/Ex3/Produto.java
@@ -0,0 +1,12 @@
+/**
+ * Representa um produto com informações básicas como nome, descrição, preço
+ * unitário e desconto.
+ */
+public class Produto {
+
+	String nome;
+	String descricao;
+	double precoUnitario;
+	double desconto;
+
+}
