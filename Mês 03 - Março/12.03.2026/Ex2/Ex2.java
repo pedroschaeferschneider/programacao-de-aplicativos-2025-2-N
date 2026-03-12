9b52b48d26c9ce4c5423ebd62adc82f637316910
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-03-12 19:27:31 -0300

    Adiciona Ex2

---
+++ Mês 03 - Março/12.03.2026/Ex2/Ex2.java
@@ -0,0 +1,35 @@
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+
+		// Criação da unidade de Blumenau
+		Mercado unidadeBlumenau = new Mercado();
+
+		unidadeBlumenau.nomeMercado = "Giassi";
+		unidadeBlumenau.qtdMacasVendidas = 500;
+		unidadeBlumenau.precoMacas = 8.7;
+		unidadeBlumenau.qtdLaranjasVendidas = 400;
+		unidadeBlumenau.precoLaranjas = 9.25;
+
+		// Criação da unidade de Joinville
+		Mercado unidadeJoinville = new Mercado();
+
+		unidadeJoinville.nomeMercado = "Angeloni";
+		unidadeJoinville.qtdMacasVendidas = 600;
+		unidadeJoinville.precoMacas = 8.9;
+		unidadeJoinville.qtdLaranjasVendidas = 300;
+		unidadeJoinville.precoLaranjas = 10.25;
+
+		// Criação da unidade de Florianopolis
+		Mercado unidadeFlorianopolis = new Mercado();
+
+		unidadeFlorianopolis.nomeMercado = "Bistek";
+		unidadeFlorianopolis.qtdMacasVendidas = 700;
+		unidadeFlorianopolis.precoMacas = 7.7;
+		unidadeFlorianopolis.qtdLaranjasVendidas = 500;
+		unidadeFlorianopolis.precoLaranjas = 11.25;
+
+	}
+
+}
---
+++ Mês 03 - Março/12.03.2026/Ex2/Mercado.java
@@ -0,0 +1,13 @@
+/**
+ * Representa um mercado que vende frutas e armazena
+ * informações sobre quantidade vendida e preços.
+ */
+public class Mercado {
+
+	String nomeMercado;
+	int qtdMacasVendidas;
+	double precoMacas;
+	int qtdLaranjasVendidas;
+	double precoLaranjas;
+
+}
